4fb39f13057abfb052b4cf1450219d0ef2e751d4
bboysquiz <mrkiraller@gmail.com>
2022-12-21 19:30:45 +0300

    finished project

--- client/thirdweb-app/src/main.jsx
+++ client/thirdweb-app/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
+import { StateContextProvider } from './context';
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThirdwebProvider desiredChainId={ChainId.Goerli}>
         <Router>
-            <App />
+            <StateContextProvider>
+                <App />
+            </StateContextProvider>
         </Router>
     </ThirdwebProvider>
 )
--- client/thirdweb-app/tailwind.config.js
+++ client/thirdweb-app/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue:
+        ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, .25)',
+      },
+    },
   },
   plugins: [],
 }
